628229185fdf4b182b0d459d20dc0f518a30ce53
brentably <brentably@gmail.com>
2024-03-15 15:01:29 -0700

    adsf

--- app/api/export-flashcards/route.ts
+++ app/api/export-flashcards/route.ts
@@ -1,36 +1,38 @@
-const genanki = require('genanki');
-const random = require('random');
+// hey alice I commented the whole thing out b/c it was fucking up the compoiler
 
-const model_id = random.randrange(1 << 30, 1 << 31);
-const model = new genanki.Model({
-    model_id, 
-    name: 'Basic Translation',
-    fields: [
-      {name: 'Target Word'},
-      {name: 'English Word'},
-    ],
-    templates: [{
-      qfmt: '{{Target Word}}',
-      afmt: '{{English Word}}',
-    }],
-});
+// const genanki = require('genanki');
+// const random = require('random');
 
-export async function POST(req: Request) {
-    try {
-        const {jsonFlashcards} = await req.json();
-        const deck_id = random.randrange(1 << 30, 1 << 31);
-        const deck = new genanki.Deck(deck_id, 'Brick German Words');
+// const model_id = random.randrange(1 << 30, 1 << 31);
+// const model = new genanki.Model({
+//     model_id, 
+//     name: 'Basic Translation',
+//     fields: [
+//       {name: 'Target Word'},
+//       {name: 'English Word'},
+//     ],
+//     templates: [{
+//       qfmt: '{{Target Word}}',
+//       afmt: '{{English Word}}',
+//     }],
+// });
 
-        let flashcards = JSON.parse(jsonFlashcards);
-        for (let i = 0; i < flashcards.length; i++) {
-            const note = new genanki.Note({
-                model: model, 
-                fields: [flashcards[i].front, flashcards[i].back]
-            });
-            deck.add_note(note);
-        }
-        await genanki.Package(deck).write_to_file('flashcards.apkg');
-    } catch (e) {
-        throw e;
-    }
-}
+// export async function POST(req: Request) {
+//     try {
+//         const {jsonFlashcards} = await req.json();
+//         const deck_id = random.randrange(1 << 30, 1 << 31);
+//         const deck = new genanki.Deck(deck_id, 'Brick German Words');
+
+//         let flashcards = JSON.parse(jsonFlashcards);
+//         for (let i = 0; i < flashcards.length; i++) {
+//             const note = new genanki.Note({
+//                 model: model, 
+//                 fields: [flashcards[i].front, flashcards[i].back]
+//             });
+//             deck.add_note(note);
+//         }
+//         await genanki.Package(deck).write_to_file('flashcards.apkg');
+//     } catch (e) {
+//         throw e;
+//     }
+// }
