51671c84cb15c61d8da3f5696f1b84cd6821964e
brentably <brentably@gmail.com>
2024-03-15 17:59:02 -0700

    streaming w/ anthropic working!

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,14 @@
 import OpenAI from 'openai';
-import {OpenAIStream, StreamingTextResponse} from 'ai';
+import {OpenAIStream, StreamingTextResponse, AnthropicStream, AIStream, readableFromAsyncIterable} from 'ai';
+import Anthropic from "@anthropic-ai/sdk";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+const client = new Anthropic({
+  apiKey: process.env.CLAUDE_API_KEY
 });
 
 
@@ -13,14 +19,31 @@ export async function POST(req: Request) {
     const latestMessage = messages[messages?.length - 1]?.content;
 
 
-    const response = await openai.chat.completions.create(
-      {
-        model: 'gpt-3.5-turbo',
-        stream: true,
-        messages: [...messages],
-      }
-    );
-    const stream = OpenAIStream(response);
+    // const response = await openai.chat.completions.create(
+    //   {
+    //     model: 'gpt-3.5-turbo',
+    //     stream: true,
+    //     messages: [...messages],
+    //   }
+    // );
+    const res = client.messages.stream({
+      model: "claude-3-opus-20240229",
+      messages: [...messages],
+      max_tokens: 4096
+    })
+    // .on('text', (text) => console.log(text))
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        for await (const chunk of res) {
+          let anyChunk:any = chunk
+          if(anyChunk?.delta?.text) controller.enqueue(anyChunk.delta.text);
+        }
+        controller.close();
+      },
+    });
+
+  
     return new StreamingTextResponse(stream);
   } catch (e) {
     throw e;
