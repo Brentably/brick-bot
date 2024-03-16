474a9f33716b0cacf01c549ba4d4b450d4194dfd
Alice Herston <aherston@berkeley.edu>
2024-03-16 03:13:38 -0700

    genanki model goated

--- app/api/export-flashcards/main.py
+++ app/api/export-flashcards/main.py
@@ -7,25 +7,9 @@ from typing import List
 
 app = FastAPI()
 
-model_id = random.randrange(1 << 30, 1 << 31)
-model = genanki.Model(
-  model_id,
-  'Flashcard',
-  fields=[
-    {'name': 'front'},
-    {'name': 'back'},
-  ],
-  templates=[
-    {
-      'name': 'Flashcard',
-      'qfmt': '{{front}}',
-      'afmt': '{{FrontSide}}<hr id="answer">{{back}}',
-    },
-  ])
-
 class Flashcard(BaseModel):
-    front: str
-    back: str
+    Front: str
+    Back: str
 
 class Flashcards(BaseModel):
     jsonFlashcards: List[Flashcard]
@@ -40,9 +24,9 @@ async def exportFlashcards(flashcards: Flashcards):
     deck = genanki.Deck(deck_id, 'Brick German Words');
     for f in flashcards.jsonFlashcards:
         note = genanki.Note(
-            model=model,
-            fields=[f.front, f.back]
+            model=genanki.BASIC_MODEL,
+            fields=[f.Front, f.Back]
         );
-        print(f.front + ' ' + f.back + '\n')
+        print(f.Front + ' ' + f.Back + '\n')
         deck.add_note(note);
     genanki.Package(deck).write_to_file('flashcards.apkg');
