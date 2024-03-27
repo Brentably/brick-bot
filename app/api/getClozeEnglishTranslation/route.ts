bab2ed0b31d431e6ca0ac26b4b5cf8bb8fc8199c
brentably <brentably@gmail.com>
2024-03-27 14:17:40 -0700

    fix type / build errors

--- app/api/getClozeEnglishTranslation/route.ts
+++ app/api/getClozeEnglishTranslation/route.ts
@@ -1,6 +1,6 @@
 import * as deepl from "deepl-node";
 
-const translator = new deepl.Translator(process.env.DEEPL_API_KEY);
+const translator = new deepl.Translator(process.env.DEEPL_API_KEY!);
 
 // (async () => {
 //   const result = await translator.translateText("Hello, world!", null, "fr");
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "Brick Bot",
   description: "Brick Bot! - Get Anki cards from a practice convo",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={GeistSans.variable}>
       <body>
-          {children}
+        {children}
       </body>
     </html>
   );
