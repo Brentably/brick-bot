aaceaec51b0af2654257ff530e47574d127b4c0d
brentably <brentably@gmail.com>
2024-03-30 14:51:38 -0700

    i am the one the bastard son dont need a gun to get respect up on the street. under the sun i am the blood

--- components/Bubble.tsx
+++ components/Bubble.tsx
@@ -41,6 +41,7 @@ const Bubble = forwardRef<HTMLDivElement, BubbleProps>(({ content, messageData, 
       bubbleAudio.current = await playAudio?.(content.content) ?? null;
     }
   };
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
   const didMakeMistakes = typeof messageData === 'undefined' || messageData === null ? null : messageData.didMakeMistakes
 
@@ -52,33 +53,62 @@ const Bubble = forwardRef<HTMLDivElement, BubbleProps>(({ content, messageData, 
           <Image src={isAudioPlaying ? soundOffIcon : soundOnIcon} alt="Sound Off Icon" />
         </button>
         : null}
-        <div className='flex flex-row'>
+      <div className='flex flex-row items-center'>
         {
           isUser ? <>
-            <div className="p-2 lg:p-4">
-              <div className="inline-block relative w-4 mr-1">
-                <span>&nbsp;</span>
-                <div className={`${didMakeMistakes === null ? 'bg-yellow-500' : didMakeMistakes ? 'bg-red-500' : 'bg-green-500'} h-4 w-4 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`} />
-              </div>
-              <strong>{messageData?.correctedMessage}</strong>
-            </div>
-            <div>
-              <Markdown
-                className="markdown grid grid-cols-1 gap-3"
-                remarkPlugins={[remarkGfm]}
-              >
-                {messageData?.correctedMessage && messageData.mistakes}
-              </Markdown>
-            </div> 
-            </> : null}
-      <div className={`rounded-[10px] ${isUser ? 'rounded-br-none text-right text-[var(--text-primary)] bg-[var(--background-bubble-primary)]' : 'rounded-bl-none text-[var(--text-secondary-inverse)] bg-[var(--background-bubble-secondary)]'} p-2 lg:p-4 leading-[1.65] pr-9 relative self-start`}>
-        <Markdown
-          className="markdown grid grid-cols-1 gap-3"
-          remarkPlugins={[remarkGfm]}
-        >
-          {content.content}
-        </Markdown>
-      </div>
+
+              {/*  */}
+              {isDropdownOpen && didMakeMistakes && (
+                <div className="fixed inset-0 z-50 flex justify-center items-center" onClick={() => setIsDropdownOpen(false)}>
+                  <div className="bg-gray-100 bg-opacity-90 border border-gray-300 rounded shadow-lg p-6 relative" style={{ width: 'calc(100% - 2rem)', maxWidth: '800px' }} onClick={e => e.stopPropagation()}>
+                    <button onClick={() => setIsDropdownOpen(false)} className="absolute top-0 right-0 mt-4 mr-4 text-gray-600 hover:text-gray-800">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                    </button>
+                    <strong className="text-lg">Corrected Message:</strong>
+                    <p className="text-md mt-2">{messageData?.correctedMessage}</p>
+                    <strong className="text-lg mt-4">Mistakes:</strong>
+                    <Markdown
+                      className="markdown text-md grid grid-cols-1 gap-2 mt-2"
+                      remarkPlugins={[remarkGfm]}
+                    >
+                      {messageData?.correctedMessage && messageData.mistakes}
+                    </Markdown>
+                  </div>
+                </div>
+              )}
+
+
+
+
+              {/*  */}
+
+
+                <button
+                  className={`${didMakeMistakes === null
+                    ? 'bg-yellow-500'
+                    : didMakeMistakes
+                      ? 'bg-red-500'
+                      : 'bg-green-500'
+                    } h-6 w-6 rounded-full focus:outline-none hover:opacity-80 transition duration-200 flex items-center justify-center mr-2`}
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                  {didMakeMistakes ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <circle cx="12" cy="12" r="10" /> <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /> <line x1="12" y1="17" x2="12.01" y2="17" /> </svg>
+                  ) : null}
+                </button>
+
+
+          </> : null}
+        <div className={`rounded-[10px] ${isUser ? 'rounded-br-none text-right text-[var(--text-primary)] bg-[var(--background-bubble-primary)]' : 'rounded-bl-none text-[var(--text-secondary-inverse)] bg-[var(--background-bubble-secondary)]'} p-2 lg:p-4 leading-[1.65] pr-9 relative self-start`}>
+          <Markdown
+            className="markdown grid grid-cols-1 gap-3"
+            remarkPlugins={[remarkGfm]}
+          >
+            {content.content}
+          </Markdown>
+        </div>
       </div>
     </div>
   )
