a16bc8cc43b6338fdcf6e0439661b3100aa85603
brentably <brentably@gmail.com>
2024-04-22 12:23:54 -0400

    asdf

--- app/api/getEnglishTranslation/route.ts
+++ app/api/getEnglishTranslation/route.ts
@@ -1,17 +1,19 @@
 import * as deepl from "deepl-node";
 
-const translator = new deepl.Translator(process.env.DEEPL_API_KEY!);
 
 // (async () => {
-//   const result = await translator.translateText("Hello, world!", null, "fr");
-//   console.log(result.text); // Bonjour, le monde !
-// })();
-
-// export const runtime = "edge"; // 'nodejs' is the default
-
-export async function POST(req: Request) {
-  try {
+  //   const result = await translator.translateText("Hello, world!", null, "fr");
+  //   console.log(result.text); // Bonjour, le monde !
+  // })();
+  
+  // export const runtime = "edge"; // 'nodejs' is the default
+  
+  export async function POST(req: Request) {
+    try {
+    const translator = new deepl.Translator(process.env.DEEPL_API_KEY!);
     console.log('getEnglish translation hit')
+    console.log('api key: ', process.env.DEEPL_API_KEY)
+    console.log('api key: ', process.env.NEXT_SERVER_DEEPL_API_KEY)
     const { sentence, language } = await req.json();
 
     console.log('translating ', sentence, ' to english')
